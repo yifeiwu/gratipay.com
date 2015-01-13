c55fcf595c812b3fd1c5621e1479de9d57cc2737
Chad Whitacre <chad@zetaweb.com>
2015-01-12 23:42:28 -0500

    Actually, I don't think we need is_anon in tips
    I think we should only divulge identity on the Receiving page for
    transfers, not for tips, in which case we don't need a new field on tips
    after all.

--- branch.sql
+++ branch.sql
@@ -1,5 +1,4 @@
 BEGIN;
     ALTER TABLE participants    ADD COLUMN anonymous_supporters boolean NOT NULL DEFAULT TRUE;
-    ALTER TABLE tips            ADD COLUMN is_anonymous         boolean NOT NULL DEFAULT TRUE;
     ALTER TABLE transfers       ADD COLUMN is_anonymous         boolean NOT NULL DEFAULT TRUE;
 END;
