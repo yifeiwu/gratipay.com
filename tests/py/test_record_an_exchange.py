bd630acf6f56ccee4f8be1ade485661580717ecd
Rohit Paul Kuruvilla <rohitpaulk@live.com>
2015-04-21 00:18:42 +0530

    Use dict for data in test_record_an_exchange.py

--- tests/py/test_record_an_exchange.py
+++ tests/py/test_record_an_exchange.py
@@ -15,50 +15,52 @@ class TestRecordAnExchange(Harness):
         self.make_participant('alice', claimed_time=now, is_admin=True)
         self.make_participant('bob', claimed_time=now)
 
-    def record_an_exchange(self, amount, fee, note, status='succeeded', make_participants=True):
+    def record_an_exchange(self, data, make_participants=True):
         if make_participants:
             self.make_participants()
-        data = {'amount': amount, 'fee': fee, 'note': note}
-        if status is not None:
-            data['status'] = status
+
+        data.setdefault('status', 'succeeded')
+        if data['status'] is None:
+            del(data['status'])
+
         return self.client.PxST('/bob/history/record-an-exchange', data, auth_as='alice')
 
     # tests
     # =====
 
     def test_success_is_302(self):
-        actual = self.record_an_exchange('10', '0', 'foo').code
+        actual = self.record_an_exchange({'amount': '10', 'fee': '0', 'note': 'foo'}).code
         assert actual == 302
 
     def test_non_admin_is_403(self):
         self.make_participant('alice', claimed_time=utcnow())
         self.make_participant('bob', claimed_time=utcnow())
-        actual = self.record_an_exchange('10', '0', 'foo', make_participants=False).code
+        actual = self.record_an_exchange({'amount': '10', 'fee': '0', 'note': 'foo'}, False).code
         assert actual == 403
 
     def test_bad_amount_is_400(self):
-        actual = self.record_an_exchange('cheese', '0', 'foo').code
+        actual = self.record_an_exchange({'amount': 'cheese', 'fee': '0', 'note': 'foo'}).code
         assert actual == 400
 
     def test_bad_fee_is_400(self):
-        actual = self.record_an_exchange('10', 'cheese', 'foo').code
+        actual = self.record_an_exchange({'amount': '10', 'fee': 'cheese', 'note': 'foo'}).code
         assert actual == 400
 
     def test_no_note_is_400(self):
-        actual = self.record_an_exchange('10', '0', '').code
+        actual = self.record_an_exchange({'amount': '10', 'fee': '0', 'note': ''}).code
         assert actual == 400
 
     def test_whitespace_note_is_400(self):
-        actual = self.record_an_exchange('10', '0', '    ').code
+        actual = self.record_an_exchange({'amount': '10', 'fee': '0', 'note': '   '}).code
         assert actual == 400
 
     def test_dropping_balance_below_zero_is_allowed_in_this_context(self):
-        self.record_an_exchange('-10', '0', 'noted')
+        self.record_an_exchange({'amount': '-10', 'fee': '0', 'note': 'foo'})
         actual = self.db.one("SELECT balance FROM participants WHERE username='bob'")
         assert actual == Decimal('-10.00')
 
     def test_success_records_exchange(self):
-        self.record_an_exchange('10', '0.50', 'noted')
+        self.record_an_exchange({'amount': '10', 'fee': '0.50', 'note': 'noted'})
         expected = { "amount": Decimal('10.00')
                    , "fee": Decimal('0.50')
                    , "participant": "bob"
@@ -71,7 +73,7 @@ class TestRecordAnExchange(Harness):
         assert actual == expected
 
     def test_success_updates_balance(self):
-        self.record_an_exchange('10', '0', 'noted')
+        self.record_an_exchange({'amount': '10', 'fee': '0', 'note': 'noted'})
         expected = Decimal('10.00')
         SQL = "SELECT balance FROM participants WHERE username='bob'"
         actual = self.db.one(SQL)
@@ -80,7 +82,11 @@ class TestRecordAnExchange(Harness):
     def test_withdrawals_work(self):
         self.make_participant('alice', claimed_time=utcnow(), is_admin=True)
         self.make_participant('bob', claimed_time=utcnow(), balance=20)
-        self.record_an_exchange('-7', '0', 'noted', make_participants=False)
+        self.record_an_exchange({
+            'amount': '-7',
+            'fee': '0',
+            'note': 'noted'
+        }, make_participants=False)
         expected = Decimal('13.00')
         SQL = "SELECT balance FROM participants WHERE username='bob'"
         actual = self.db.one(SQL)
@@ -89,19 +95,24 @@ class TestRecordAnExchange(Harness):
     def test_withdrawals_take_fee_out_of_balance(self):
         self.make_participant('alice', claimed_time=utcnow(), is_admin=True)
         self.make_participant('bob', claimed_time=utcnow(), balance=20)
-        self.record_an_exchange('-7', '1.13', 'noted', make_participants=False)
+        self.record_an_exchange({'amount': '-7', 'fee': '1.13', 'note': 'noted'}, False)
         SQL = "SELECT balance FROM participants WHERE username='bob'"
         assert self.db.one(SQL) == Decimal('11.87')
 
     def test_can_set_status(self):
         self.make_participants()
         for status in ('pre', 'pending', 'failed', 'succeeded'):
-            self.record_an_exchange('10', '0', 'noted', status, False)
+            self.record_an_exchange({
+                'amount': '10',
+                'fee': '0',
+                'note': 'noted',
+                'status': status
+            }, False)
             actual = self.db.one("SELECT status FROM exchanges ORDER BY timestamp desc LIMIT 1")
             assert actual == status
 
     def test_cant_record_new_exchanges_with_None_status(self):
-        r = self.record_an_exchange('10', '0', 'noted', None)
+        r = self.record_an_exchange({'amount': '10', 'fee': '0', 'note': 'noted', 'status': None})
         assert r.code == 400
         assert self.db.one("SELECT count(*) FROM exchanges") == 0
 
@@ -109,26 +120,41 @@ class TestRecordAnExchange(Harness):
         self.make_participants()
         balance = 0
         for amount in ('10', '-10'):
-            self.record_an_exchange(amount, '0', 'noted', 'succeeded', False)
+            self.record_an_exchange({'amount': amount, 'fee': '0', 'note': 'noted'}, False)
             balance += int(amount)
             assert self.db.one("SELECT balance FROM participants WHERE username='bob'") == balance
 
     def test_failed_doesnt_affect_balance(self):
         self.make_participants()
         for amount in ('10', '-10'):
-            self.record_an_exchange(amount, '0', 'noted', 'failed', False)
+            self.record_an_exchange({
+                'amount': amount,
+                'fee': '0',
+                'note': 'noted',
+                'status': 'failed'
+            }, False)
             assert self.db.one("SELECT balance FROM participants WHERE username='bob'") == 0
 
     def test_other_statuses_dont_affect_balance_for_payins(self):
         self.make_participants()
         for status in ('pre', 'pending'):
-            self.record_an_exchange('10', '0', 'noted', status, False)
+            self.record_an_exchange({
+                'amount': '10',
+                'fee': '0',
+                'note': 'noted',
+                'status': status
+            }, False)
             assert self.db.one("SELECT balance FROM participants WHERE username='bob'") == 0
 
     def test_other_statuses_affect_balance_for_payouts(self):
         self.make_participants()
         balance = 0
         for status in ('pre', 'pending'):
-            self.record_an_exchange('-10', '0', 'noted', status, False)
+            self.record_an_exchange({
+                'amount': '-10',
+                'fee': '0',
+                'note': 'noted',
+                'status': status
+            }, False)
             balance -= 10
             assert self.db.one("SELECT balance FROM participants WHERE username='bob'") == balance
