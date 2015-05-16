4a1a88c661360fdd2ed14669cc84c3c2c21591e1
Rohit Paul Kuruvilla <rohitpaulk@live.com>
2015-05-16 16:00:20 +0530

    Copy over functions related to takes

--- gratipay/models/team.py
+++ gratipay/models/team.py
@@ -85,6 +85,76 @@ class Team(Model):
                , True: 'approved'
                 }[self.is_approved]
 
+    # Members
+    # =======
+
+    def add_member(self, member):
+        """Add a member to this team.
+        """
+        assert self.IS_PLURAL
+        if len(self.get_current_takes()) == 149:
+            raise MemberLimitReached
+        if not member.is_claimed:
+            raise StubParticipantAdded
+        self.__set_take_for(member, Decimal('0.01'), self)
+
+    def remove_member(self, member):
+        """Remove a member from this team.
+        """
+        assert self.IS_PLURAL
+        self.__set_take_for(member, Decimal('0.00'), self)
+
+    def remove_all_members(self, cursor=None):
+        (cursor or self.db).run("""
+            INSERT INTO takes (ctime, member, team, amount, recorder) (
+                SELECT ctime, member, %(username)s, 0.00, %(username)s
+                  FROM current_takes
+                 WHERE team=%(username)s
+                   AND amount > 0
+            );
+        """, dict(username=self.username))
+
+    @property
+    def nmembers(self):
+        assert self.IS_PLURAL
+        return self.db.one("""
+            SELECT COUNT(*)
+              FROM current_takes
+             WHERE team=%s
+        """, (self.username, ))
+
+    def get_members(self, current_participant=None):
+        """Return a list of member dicts.
+        """
+        assert self.IS_PLURAL
+        takes = self.compute_actual_takes()
+        members = []
+        for take in takes.values():
+            member = {}
+            member['username'] = take['member']
+            member['take'] = take['nominal_amount']
+            member['balance'] = take['balance']
+            member['percentage'] = take['percentage']
+
+            member['removal_allowed'] = current_participant == self
+            member['editing_allowed'] = False
+            member['is_current_user'] = False
+            if current_participant is not None:
+                if member['username'] == current_participant.username:
+                    member['is_current_user'] = True
+                    if take['ctime'] is not None:
+                        # current user, but not the team itself
+                        member['editing_allowed']= True
+
+            member['last_week'] = last_week = self.get_take_last_week_for(member)
+            member['max_this_week'] = self.compute_max_this_week(last_week)
+            members.append(member)
+        return members
+
+
+    # Takes
+    # =====
+
     def get_take_last_week_for(self, member):
         """Get the user's nominal take last week. Used in throttling.
         """
