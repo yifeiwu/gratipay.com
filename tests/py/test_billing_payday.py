aa3aade6724ece5a42e8f2824196041d54a43f8f
Rohit Paul Kuruvilla <rohitpaulk@live.com>
2015-05-29 00:06:41 +0530

    Add pytest import to test_billing_payday.py

--- tests/py/test_billing_payday.py
+++ tests/py/test_billing_payday.py
@@ -6,6 +6,7 @@ import os
 import balanced
 import braintree
 import mock
+import pytest
 
 from gratipay.billing.exchanges import create_card_hold
 from gratipay.billing.payday import NoPayday, Payday
